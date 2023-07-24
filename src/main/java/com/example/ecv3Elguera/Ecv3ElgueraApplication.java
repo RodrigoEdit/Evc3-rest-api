55b422eeaca4dab78cb383691756e76f901f2d91
Rodrigo_Elguera <111022228+RodrigoEdit@users.noreply.github.com>
2023-07-24 16:36:23 +0000

    Commit All

---
+++ src/main/java/com/example/ecv3Elguera/Ecv3ElgueraApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecv3Elguera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecv3ElgueraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecv3ElgueraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecv3Elguera/Ecv3ElgueraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecv3Elguera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecv3ElgueraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
